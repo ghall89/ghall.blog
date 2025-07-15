cd92b68ab5cd686ebb5c746f1c397dbedc0e3e60
Graham Hall <graham@ghall.dev>
2025-07-15 10:32:48 -0400

    ➕ Setup Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import mdx from '@astrojs/mdx';
-
+import tailwind from '@astrojs/tailwind';
 import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ghall.space',
 	output: 'static',
-	integrations: [mdx(), alpinejs()],
+	integrations: [mdx(), alpinejs(), tailwind()],
 	experimental: {
 		svg: true,
 	},
